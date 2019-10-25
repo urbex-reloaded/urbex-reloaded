8e704d8e0f05c87cae4aba9dd76038916baf7f24
FajsiEx <fajsiexofficial@gmail.com>
2019-10-25 21:57:20 +0200

    / fix tslint problems with the code quality

--- src/app/pages/home/section-events/section-events.component.ts
+++ src/app/pages/home/section-events/section-events.component.ts
@@ -9,19 +9,21 @@ export class SectionEventsComponent implements OnInit {
 
   events = [
     {
-      endDate: new Date("11/11/2019 6:00:00").getTime(),
-      title: "Some place idk",
-      description: "Some event description idk lorem ipsum idk lulw"
+      endDate: new Date('11/11/2019 6:00:00').getTime(),
+      title: 'Some place idk',
+      description: 'Some event description idk lorem ipsum idk lulw'
     },
     {
-      endDate: new Date("11/11/2019 6:00:00").getTime(),
-      title: "Some place idk",
-      description: "Some event description idk lorem ipsum idk lulw jshdjkshjdkshkdskajdhskahdkjsahdkjkjdsdsahkdhakjdhsjkhdkjsadkjshkjdsakjdhsakjhdsjhdkjsahdkjsahkjdsahkjdshkjdsk"
+      endDate: new Date('11/11/2019 6:00:00').getTime(),
+      title: 'Some place idk',
+      // tslint:disable-next-line:max-line-length
+      description: 'Some event description idk lorem ipsum idk lulw jshdjkshjdkshkdskajdhskahdkjsahdkjkjdsdsahkdhakjdhsjkhdkjsadkjshkjdsakjdhsakjhdsjhdkjsahdkjsahkjdsahkjdshkjdsk'
     },
     {
-      endDate: new Date("11/11/2019 6:00:00").getTime(),
-      title: "Some place idk",
-      description: "Some event description idk lorem ipsum idk lulw"
+      endDate: new Date('11/11/2019 6:00:00').getTime(),
+      title: 'Dunno name',
+      // tslint:disable-next-line:max-line-length
+      description: '> As for science, it may be negative.\nHowever, do not forget the thing that there is a scientific side in all the one.\nThe important one is true.\nOf me the ruler my fate, and me also the commander of my soul.'
     },
   ];
 
